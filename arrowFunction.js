4daf3bd061ac16f50fa76a8bd56417e6081cf26a
Trey Zima <trey.m.zima@gmail.com>
2024-11-16 16:04:38 -0700

    Completed Task 3

--- arrowFunction.js
+++ arrowFunction.js
@@ -13,6 +13,9 @@ console.log(scanForLife());
 /* Task 3: Implicit Return with Objects: Log Coordinates */
 // TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
 
+const currentCoordinates = (x, y, z) => ({ x, y, z });
+console.log(currentCoordinates(57, 23, -12));
+
 /* Task 4: Understanding `this`: Message from Home Base */
 // TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
 /*
